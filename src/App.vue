f31dedde17187154e786d6e80d60cac13aa074fa
Pedro Santana Neves <pneves@bladeinsight.com>
2022-12-02 17:16:28 +0000

    feat: add js logic

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,42 @@
 <script setup>
+import { ref, computed } from "vue";
+
+const tipsOptions = [5, 10, 15, 20];
+const bill = ref(null);
+const numberOfPeople = ref(null);
+const tipPercent = ref(null);
+const formatter = new Intl.NumberFormat("pt-PT", {
+  style: "currency",
+  currency: "EUR",
+  maximumFractionDigits: 2,
+  minimumFractionDigits: 2,
+});
+
+const tipAmountPerPerson = computed(() => {
+  const amount = (bill.value * (tipPercent.value / 100)) / numberOfPeople.value;
+  return isNaN(amount) || !numberOfPeople.value ? 0 : amount;
+});
+const tipAmountPerPersonDisplay = computed(() => {
+  return formatter.format(tipAmountPerPerson.value);
+});
+
+const totalAmountPerPerson = computed(() => {
+  const amount =
+    (bill.value * (tipPercent.value / 100 + 1)) / numberOfPeople.value;
+  return isNaN(amount) || !numberOfPeople.value ? 0 : amount;
+});
+const totalAmountPerPersonDisplay = computed(() => {
+  return formatter.format(totalAmountPerPerson.value);
+});
+
 const pageTitle = `Bill Splitter`;
+const reset = () => {
+  bill.value = null;
+  numberOfPeople.value = null;
+};
+const setTipPercent = (value) => {
+  tipPercent.value = tipPercent.value === value ? null : value;
+};
 </script>
 
 <template>
@@ -9,28 +46,29 @@ const pageTitle = `Bill Splitter`;
       <div id="billForm" class="col">
         <label class="input">
           Bill
-          <input type="number" />
+          <input type="number" v-model="bill" placeholder="0" />
         </label>
         <div>
           <div id="tipSelectorTitle">Tip %</div>
           <div id="tipSelector" class="row">
-            <div class="col">
-              <button>5%</button>
-            </div>
-            <div class="col">
-              <button>10%</button>
-            </div>
-            <div class="col">
-              <button class="active">15%</button>
-            </div>
-            <div class="col">
-              <button>20%</button>
+            <div
+              v-for="(option, index) in tipsOptions"
+              :key="`tip-${index}`"
+              class="col"
+            >
+              <button
+                type="button"
+                :class="{ active: option === tipPercent }"
+                @click="setTipPercent(option)"
+              >
+                {{ option }}%
+              </button>
             </div>
           </div>
         </div>
         <label class="input">
           Number of People
-          <input type="number" />
+          <input type="number" v-model="numberOfPeople" placeholder="0" />
         </label>
       </div>
       <div class="col">
@@ -38,14 +76,14 @@ const pageTitle = `Bill Splitter`;
           <div id="outputValuesContainer">
             <div>
               <div class="outputLabel">Tip Amount <small>/ person</small></div>
-              <div class="outputValue">123.00€</div>
+              <div class="outputValue">{{ tipAmountPerPersonDisplay }}</div>
             </div>
             <div>
               <div class="outputLabel">Total <small>/ person</small></div>
-              <div class="outputValue">250.99€</div>
+              <div class="outputValue">{{ totalAmountPerPersonDisplay }}</div>
             </div>
           </div>
-          <button id="resetButton">Reset</button>
+          <button id="resetButton" type="button" @click="reset">Reset</button>
         </div>
       </div>
     </div>
